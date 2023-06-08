1342205b7d860751fa012ed3e0abdc55dc534460
Nabeel Khan <nabeekha@umich.edu>
2023-06-08 07:56:34 -0400

    commit with component bases

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Header from './components/Header'
 
 function App() {
   return (
-    <div>App</div>
+    <>
+    <Header/>
+    <Navbar/>
+    </>
   )
 }
 
