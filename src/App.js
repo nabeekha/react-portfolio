0b5d2a61a90885a5eeeb84c8193394932bbe37dc
Nabeel Khan <nabeekha@umich.edu>
2023-07-03 21:12:35 -0400

    changing app.js files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from './components/Navbar'
+import Preloader from "../src/components/Pre";
+import Resume from "./components/Resume/Resume";
 import Header from './components/Header'
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, Routes,Navigate} from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
+import "./style.css";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [load, newLoad] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+        newLoad(false)
+    }, 1000)
+  })
+
   return (
-    <>
-    <Header/>
-    <Navbar/>
-    </>
-  )
+    <Router>
+      <Preloader load={load} />
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Header/>} />
+          <Route path="/project" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="*" element={<Navigate to="/"/>} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App
