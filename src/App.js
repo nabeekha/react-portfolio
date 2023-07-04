7110866116028cac9e76122c7875c1d596ce6e89
Nabeel Khan <nabeekha@umich.edu>
2023-07-03 21:27:38 -0400

    adding topscroll component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import Navbar from './components/Navbar'
 import Preloader from "../src/components/Pre";
-import Resume from "./components/Resume/Resume";
+import Resume from "./components/Resume";
 import Header from './components/Header'
-import About from "./components/About/About";
-import Projects from "./components/Projects/Projects";
+import About from "./components/AboutMe";
+import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Route, Routes,Navigate} from "react-router-dom";
-import ScrollToTop from "./components/ScrollToTop";
+import TopScroll from "./components/TopScroll";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -26,7 +26,7 @@ function App() {
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
-        <ScrollToTop />
+        <TopScroll />
         <Routes>
           <Route path="/" element={<Header/>} />
           <Route path="/project" element={<Projects />} />
